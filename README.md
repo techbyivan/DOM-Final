3bb02331163c2da412a891a3bedec4273c05271f
Ivan Davis <techbyivan@users.noreply.github.com>
2020-06-29 08:10:39 -0400

    Fixing Logic & Deleted Files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DOM-Final
+## Classic Card Game - War
--- code2.js
+++ code2.js
@@ -7,21 +7,36 @@
 //         txt = `Welcome ${player}!`
 //     }
 //     document.getElementById('username').innerHTML = txt
-}
+// }
 
-dealCards = () => {
-    document.getElementById('player1Card').innerHTML = Math.floor(Math.random() * 11);
-    document.getElementById('houseCard').innerHTML = Math.floor(Math.random() * 11);
-    let cardGenerated = player1card;
-    let cardGenerated2 = houseCard;
-    if (player1card > houseCard) {
-        document.getElementById('player-1-score).innerHTML = cardGenerated;
-    } else {
+// dealCards = () => {
+//     document.getElementById('player1Card').innerHTML = Math.floor(Math.random() * 11);
+//     document.getElementById('houseCard').innerHTML = Math.floor(Math.random() * 11);
+//     let cardGenerated = player1card;
+//     let cardGenerated2 = houseCard;
+//     if (player1card > houseCard) {
+//         document.getElementById('player-1-score).innerHTML = cardGenerated;
+//     } else {
         
-    }
-}
-console.log(dealCards())
+//     }
+// }
+// console.log(dealCards())
 
+// FLIPPED CARD START
+
+// let card = document.querySelector('.card');
+// card.addEventListener( 'click', function() {
+    //   card.classList.toggle('is-flipped');
+    // });
+    
+// FLIPPED CARD END
+
+
+
+// var card = document.querySelector('.card');
+// card.addEventListener( 'click', function() {
+//   card.classList.toggle('is-flipped');
+// });
 
 
 // let player = 'Will Smith';
@@ -53,4 +68,3 @@ console.log(dealCards())
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
-
