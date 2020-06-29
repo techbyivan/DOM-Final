db1a3122ec0c948b0a55cfa36fb696d528cda4f7
Ivan Davis <techbyivan@users.noreply.github.com>
2020-06-29 00:39:58 -0400

    Uploaded New Cards

--- code2.js
+++ code2.js
@@ -1,24 +1,56 @@
-let highScore = ;
-let username = 'Will Smith';
-let rounds = [];
-let p = 45;
-let reset = 0; 
-let h = 309;
-let pLight = ; 
-let hLight = ;
-let p1Card = ;
-let deal = ;
-let hCard = ;
+// getUsername = () => {
+//     let txt;
+//     let player = prompt("Please enter your name:")
+//     if (player == null || player == "") {
+//         txt = "User cancelled the prompt."
+//     } else {
+//         txt = `Welcome ${player}!`
+//     }
+//     document.getElementById('username').innerHTML = txt
+}
 
-
-
-comparison = (p, h) => {
-    if (p > h) {
-        return `${username} Wins!`
+dealCards = () => {
+    document.getElementById('player1Card').innerHTML = Math.floor(Math.random() * 11);
+    document.getElementById('houseCard').innerHTML = Math.floor(Math.random() * 11);
+    let cardGenerated = player1card;
+    let cardGenerated2 = houseCard;
+    if (player1card > houseCard) {
+        document.getElementById('player-1-score).innerHTML = cardGenerated;
     } else {
-        return 'House Wins!'
+        
     }
-};
+}
+console.log(dealCards())
 
-console.log(comparison(p,h))
+
+
+// let player = 'Will Smith';
+// let playerPoints = 30
+// let highScore = `${player} is the High Score with ${playerPoints} points!!`
+
+// let playerPoints = ;
+// let playerCardsLeft = 26;
+// let playerGamesWon = 0
+
+// let housePoints = ;
+// let houseCardsLeft = 26;
+// let houseGamesWon = 0
+
+// let rounds = [];
+// let p = 932
+// let reset = 0; 
+// let h = 309;
+
+// let playerSensor = ; 
+// let houseSensor = ;
+
+// console.log(highScore)
+
+
+
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
 
